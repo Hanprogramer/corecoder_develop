a8a3ce988d9c36916329e87bb102328a110ae8eb
Han Programer <han.programer@gmail.com>
2021-12-13 10:49:56 +0700

    Refactor: JS Module and JS Api implementation are now separated

---
+++ lib/modules/jsapi.dart
@@ -0,0 +1,234 @@
+import 'dart:convert';
+import 'dart:ffi';
+import 'dart:typed_data';
+import 'package:corecoder_develop/modules/module_jsplugins.dart';
+import 'package:corecoder_develop/util/modules_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:ffi/ffi.dart';
+import 'package:flutter_jscore/flutter_jscore.dart';
+import 'package:flutter_jscore/jscore_bindings.dart' as js;
+
+
+String jsStringToDartString(Pointer resultJsString) {
+  var resultCString = js.jSStringGetCharactersPtr(resultJsString);
+  int resultCStringLength = js.jSStringGetLength(resultJsString);
+  if (resultCString == nullptr) {
+    return 'null';
+  }
+  String result = String.fromCharCodes(Uint16List.view(
+      resultCString.cast<Uint16>().asTypedList(resultCStringLength).buffer,
+      0,
+      resultCStringLength));
+  return result;
+}
+
+String _getJsValue(Pointer _ctxPtr, Pointer jsValueRef) {
+  if (js.jSValueIsNull(_ctxPtr, jsValueRef) == 1) {
+    return 'null';
+  } else if (js.jSValueIsUndefined(_ctxPtr, jsValueRef) == 1) {
+    return 'undefined';
+  } else if (js.jSValueIsObject(_ctxPtr, jsValueRef) == 1) {
+    // Is object, convert to map
+    return 'Object object';
+  }
+
+  /// Last resort, cast anything to string, like "[Object object]"
+  var resultJsString = js.jSValueToStringCopy(_ctxPtr, jsValueRef, nullptr);
+  var resultCString = js.jSStringGetCharactersPtr(resultJsString);
+  int resultCStringLength = js.jSStringGetLength(resultJsString);
+  if (resultCString == nullptr) {
+    return 'null';
+  }
+  String result = String.fromCharCodes(Uint16List.view(
+      resultCString.cast<Uint16>().asTypedList(resultCStringLength).buffer,
+      0,
+      resultCStringLength));
+  js.jSStringRelease(resultJsString);
+  return result;
+}
+
+/// Convert a javascript object to dart map
+/// [jsValueRef] The pointer to JS obj
+Map<String, String> jsObjectToDartMap(Pointer _ctxPtr, Pointer jsValueRef) {
+  if (js.jSValueIsObject(_ctxPtr, jsValueRef) == 1) {
+    // if is object
+    Pointer obj = js.jSValueToObject(_ctxPtr, jsValueRef, nullptr);
+
+    //(JSPropertyNameArrayRef)
+    Pointer props = js.jSObjectCopyPropertyNames(_ctxPtr, obj);
+    int propCount = js.jSPropertyNameArrayGetCount(props);
+    Map<String, String> result = {};
+    // debugPrint("JS PROPC $propCount");
+    for (var i = 0; i < propCount; i++) {
+      Pointer /*(JSStringRef)*/ propName =
+      js.jSPropertyNameArrayGetNameAtIndex(props, i);
+      Pointer /*JSValueRef*/ propValue =
+      js.jSObjectGetProperty(_ctxPtr, obj, propName, nullptr);
+      String name = jsStringToDartString(propName);
+      js.jSStringRelease(propName);
+      dynamic value;
+      int propType = js.jSValueGetType(_ctxPtr, propValue);
+      switch (propType) {
+        case 4: //kJSTypeString:
+          value = _getJsValue(_ctxPtr, propValue);
+          break;
+        case 3: //kJSTypeNumber:
+        //TODO:not implemented
+          break;
+      }
+      // debugPrint("JS NAME $name");
+      // debugPrint("JS VALUE($propType) $value");
+      result[name] = value;
+    }
+    return result;
+  }
+  int type = js.jSValueGetType(_ctxPtr, jsValueRef);
+  debugPrint("jsObjectToDartMap error: value is not obj, type:$type");
+  return {};
+}
+
+
+class CoreCoder {
+  static late JsModule module; // set by the parent object
+  static late JSContext context; // set by the parent object
+  static CoreCoder? _instance;
+  static CoreCoder get instance{
+    _instance ??= CoreCoder();
+    _printDartFunc = _instance!._print;
+    return _instance!;
+  }
+  
+  void addTemplate(Pointer ctx,
+      Pointer function,
+      Pointer thisObject,
+      int argumentCount,
+      Pointer<Pointer> arguments,
+      Pointer<Pointer> exception){
+    if (argumentCount > 0) {
+      Pointer jsValueRef = arguments[0];
+      if (js.jSValueIsObject(ctx, jsValueRef) == 1) {
+        /// the provided argument 0 is an object, then parse it
+        String name, description, version, identifier;
+        Pointer obj = js.jSValueToObject(ctx, jsValueRef, nullptr);
+        name = _getJsValue(ctx,js.jSObjectGetProperty(ctx, obj,
+            js.jSStringCreateWithUTF8CString('name'.toNativeUtf8()), nullptr));
+
+        description = _getJsValue(ctx,js.jSObjectGetProperty(
+            ctx,
+            obj,
+            js.jSStringCreateWithUTF8CString('description'.toNativeUtf8()),
+            nullptr));
+
+        version = _getJsValue(ctx,js.jSObjectGetProperty(ctx, obj,
+            js.jSStringCreateWithUTF8CString('version'.toNativeUtf8()), nullptr));
+
+        identifier = _getJsValue(ctx,js.jSObjectGetProperty(
+            ctx,
+            obj,
+            js.jSStringCreateWithUTF8CString('identifier'.toNativeUtf8()),
+            nullptr));
+
+        var _options = js.jSObjectGetProperty(ctx, obj,
+            js.jSStringCreateWithUTF8CString('options'.toNativeUtf8()), nullptr);
+        var options = jsObjectToDartMap(ctx,_options);
+
+        var onCreate = (Map<String, dynamic> args) async {};
+        var _onCreate = js.jSObjectGetProperty(ctx, obj,
+            js.jSStringCreateWithUTF8CString('onCreate'.toNativeUtf8()), nullptr);
+        if (js.jSValueIsObject(ctx, _onCreate) == 1) {
+          onCreate = (Map<String, dynamic> args) async {
+            var optionsObj = JSObject.make(
+                context,
+                JSClass.create(
+                    JSClassDefinition(className: "OptionsObj")));
+            for (var key in args.keys) {
+              var value = args[key];
+              if (value is String) {
+                optionsObj.setProperty(
+                    key,
+                    JSValue.makeString(context, value),
+                    JSPropertyAttributes.kJSPropertyAttributeReadOnly);
+              } else if (value is int || value is double) {
+                optionsObj.setProperty(
+                    key,
+                    JSValue.makeNumber(context, value as double),
+                    JSPropertyAttributes.kJSPropertyAttributeReadOnly);
+              }
+            }
+            js.jSObjectCallAsFunction(
+                ctx,
+                _onCreate,
+                obj,
+                1,
+                JSValuePointer.array([optionsObj.toValue()]).pointer,
+                nullptr);
+          };
+        }
+
+        module.templates.add(Template(
+          name,
+          description,
+          version,
+          options,
+          onCreate,
+          module.icon,
+          identifier,
+        ));
+      }
+    }
+  }
+  
+  static Pointer jsAddTemplate(Pointer ctx,
+      Pointer function,
+      Pointer thisObject,
+      int argumentCount,
+      Pointer<Pointer> arguments,
+      Pointer<Pointer> exception){
+    instance.addTemplate(ctx,function, thisObject, argumentCount, arguments, exception);
+    return nullptr;
+  }
+  
+  /*static JSObjectCallAsFunctionCallbackDart? get jsAddTemplate{
+    return _jsAddTemplate;  
+  }*/
+
+
+  /// # ======== THE PRINT FUNCTION ========== # ///
+  static Pointer flutterPrint(
+      Pointer ctx,
+      Pointer function,
+      Pointer thisObject,
+      int argumentCount,
+      Pointer<Pointer> arguments,
+      Pointer<Pointer> exception) {
+    if (_printDartFunc != null) {
+      _printDartFunc!(
+          ctx, function, thisObject, argumentCount, arguments, exception);
+    }
+    return nullptr;
+  }
+
+  static JSObjectCallAsFunctionCallbackDart?
+  _printDartFunc; // points to non static function
+
+  Pointer _print(
+      Pointer ctx,
+      Pointer function,
+      Pointer thisObject,
+      int argumentCount,
+      Pointer<Pointer> arguments,
+      Pointer<Pointer> exception) {
+    if (argumentCount > 0) {
+      debugPrint(_getJsValue(ctx,arguments[0]));
+    }
+    return nullptr;
+  }
+
+/// # ======== THE PRINT FUNCTION - END ========== # ///
+  
+}
+
+class FileIO{
+  static late JsModule module; // set by the parent object
+  static late JSContext context; // set by the parent object
+}
--- lib/modules/module_jsplugins.dart
+++ lib/modules/module_jsplugins.dart
@@ -1,329 +1,45 @@
-import 'dart:convert';
 import 'dart:ffi';
 import 'dart:typed_data';
+import 'package:corecoder_develop/modules/jsapi.dart';
 import 'package:corecoder_develop/util/modules_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_jscore/jscore_bindings.dart';
 import 'package:ffi/ffi.dart';
 import 'package:flutter_jscore/flutter_jscore.dart' as jscore
     show
-        JSValuePointer,
-        JSStringPointer,
-        JSValue,
         JSContext,
         JSContextGroup,
         JSObject,
-        JSObjectPointer,
         JSClass,
         JSClassDefinition,
-        JSPropertyAttributes;
+        JSPropertyAttributes,
+        JSClassAttributes,
+        JSStaticFunction;
 
 class JsModule extends Module {
   String mainScript = "";
 
-  // Pointer _context;
-  // JSContext? get jsContext{
-  //   _context ??= jSGlobalContextCreateInGroup();
-  //   return _context;
-  // }
-  late jscore.JSContext _globalContext;
-  late Pointer contextGroup;
+  late jscore.JSContext context;
+  late jscore.JSObject globalObj;
 
-  Pointer get globalContext => _globalContext.pointer;
-  late Pointer globalObject;
+  Pointer get _ctxPtr => context.pointer;
+  late Pointer _globalObjPtr;
 
-  static Pointer alert(
-      Pointer ctx,
-      Pointer function,
-      Pointer thisObject,
-      int argumentCount,
-      Pointer<Pointer> arguments,
-      Pointer<Pointer> exception) {
-    if (_alertDartFunc != null) {
-      _alertDartFunc!(
-          ctx, function, thisObject, argumentCount, arguments, exception);
-    }
-    return nullptr;
-  }
-
-  static JSObjectCallAsFunctionCallbackDart? _alertDartFunc;
-
-  Pointer _alert(
-      Pointer ctx,
-      Pointer function,
-      Pointer thisObject,
-      int argumentCount,
-      Pointer<Pointer> arguments,
-      Pointer<Pointer> exception) {
-    String msg = 'No Message';
-    if (argumentCount != 0) {
-      msg = '';
-      for (int i = 0; i < argumentCount; i++) {
-        if (i != 0) {
-          msg += '\n';
-        }
-        var jsValueRef = arguments[i];
-        msg += _getJsValue(jsValueRef);
-      }
-    }
-    // showDialog(
-    //     context: context,
-    //     builder: (context) {
-    //       return AlertDialog(
-    //         title: Text('Alert'),
-    //         content: Text(msg),
-    //       );
-    //     });
-    return nullptr;
-  }
-
-  String jsStringToDartString(Pointer resultJsString) {
-    var resultCString = jSStringGetCharactersPtr(resultJsString);
-    int resultCStringLength = jSStringGetLength(resultJsString);
-    if (resultCString == nullptr) {
-      return 'null';
-    }
-    String result = String.fromCharCodes(Uint16List.view(
-        resultCString.cast<Uint16>().asTypedList(resultCStringLength).buffer,
-        0,
-        resultCStringLength));
-    return result;
-  }
-
-  String _getJsValue(Pointer jsValueRef) {
-    if (jSValueIsNull(globalContext, jsValueRef) == 1) {
-      return 'null';
-    } else if (jSValueIsUndefined(globalContext, jsValueRef) == 1) {
-      return 'undefined';
-    } else if (jSValueIsObject(globalContext, jsValueRef) == 1) {
-      // Is object, convert to map
-      return 'Object object';
-    }
-
-    /// Last resort, cast anything to string, like "[Object object]"
-    var resultJsString =
-        jSValueToStringCopy(globalContext, jsValueRef, nullptr);
-    var resultCString = jSStringGetCharactersPtr(resultJsString);
-    int resultCStringLength = jSStringGetLength(resultJsString);
-    if (resultCString == nullptr) {
-      return 'null';
-    }
-    String result = String.fromCharCodes(Uint16List.view(
-        resultCString.cast<Uint16>().asTypedList(resultCStringLength).buffer,
-        0,
-        resultCStringLength));
-    jSStringRelease(resultJsString);
-    return result;
-  }
-
-  /// # ======== THE PRINT FUNCTION ========== # ///
-  static Pointer flutterPrint(
-      Pointer ctx,
-      Pointer function,
-      Pointer thisObject,
-      int argumentCount,
-      Pointer<Pointer> arguments,
-      Pointer<Pointer> exception) {
-    if (_printDartFunc != null) {
-      _printDartFunc!(
-          ctx, function, thisObject, argumentCount, arguments, exception);
-    }
-    return nullptr;
-  }
-
-  static JSObjectCallAsFunctionCallbackDart? _printDartFunc;
-
-  Pointer _print(
-      Pointer ctx,
-      Pointer function,
-      Pointer thisObject,
-      int argumentCount,
-      Pointer<Pointer> arguments,
-      Pointer<Pointer> exception) {
-    if (argumentCount > 0) {
-      debugPrint(_getJsValue(arguments[0]));
-    }
-    return nullptr;
-  }
-
-  /// # ======== THE PRINT FUNCTION - END ========== # ///
-
-  /// # ======== THE addTemplate FUNCTION ========== # ///
-  static Pointer ccAddTemplate(
-      Pointer ctx,
-      Pointer function,
-      Pointer thisObject,
-      int argumentCount,
-      Pointer<Pointer> arguments,
-      Pointer<Pointer> exception) {
-    if (_ccAddTemplateFunc != null) {
-      _ccAddTemplateFunc!(
-          ctx, function, thisObject, argumentCount, arguments, exception);
-    }
-    return nullptr;
-  }
-
-  static JSObjectCallAsFunctionCallbackDart? _ccAddTemplateFunc;
-
-  /// Convert a javascript object to dart map
-  /// [jsValueRef] The pointer to JS obj
-  Map<String, String> jsObjectToDartMap(Pointer jsValueRef) {
-    if (jSValueIsObject(globalContext, jsValueRef) == 1) {
-      // if is object
-      Pointer obj = jSValueToObject(globalContext, jsValueRef, nullptr);
-
-      //(JSPropertyNameArrayRef)
-      Pointer props = jSObjectCopyPropertyNames(globalContext, obj);
-      int propCount = jSPropertyNameArrayGetCount(props);
-      Map<String, String> result = {};
-      // debugPrint("JS PROPC $propCount");
-      for (var i = 0; i < propCount; i++) {
-        Pointer /*(JSStringRef)*/ propName =
-            jSPropertyNameArrayGetNameAtIndex(props, i);
-        Pointer /*JSValueRef*/ propValue =
-            jSObjectGetProperty(globalContext, obj, propName, nullptr);
-        String name = jsStringToDartString(propName);
-        jSStringRelease(propName);
-        dynamic value;
-        int propType = jSValueGetType(globalContext, propValue);
-        switch (propType) {
-          case 4: //kJSTypeString:
-            value = _getJsValue(propValue);
-            break;
-          case 3: //kJSTypeNumber:
-            //TODO:not implemented
-            break;
-        }
-        // debugPrint("JS NAME $name");
-        // debugPrint("JS VALUE($propType) $value");
-        result[name] = value;
-      }
-      return result;
-    }
-    int type = jSValueGetType(globalContext, jsValueRef);
-    debugPrint("jsObjectToDartMap error: value is not obj, type:$type");
-    return {};
-  }
-
-  Pointer _ccAddTemplate(
-      Pointer ctx,
-      Pointer function,
-      Pointer thisObject,
-      int argumentCount,
-      Pointer<Pointer> arguments,
-      Pointer<Pointer> exception) {
-    if (argumentCount > 0) {
-      Pointer jsValueRef = arguments[0];
-      if (jSValueIsObject(globalContext, jsValueRef) == 1) {
-        /// the provided argument 0 is an object, then parse it
-        String name, description, version, identifier;
-        Pointer obj = jSValueToObject(globalContext, jsValueRef, nullptr);
-        name = _getJsValue(jSObjectGetProperty(globalContext, obj,
-            jSStringCreateWithUTF8CString('name'.toNativeUtf8()), nullptr));
-        description = _getJsValue(jSObjectGetProperty(
-            globalContext,
-            obj,
-            jSStringCreateWithUTF8CString('description'.toNativeUtf8()),
-            nullptr));
-        version = _getJsValue(jSObjectGetProperty(globalContext, obj,
-            jSStringCreateWithUTF8CString('version'.toNativeUtf8()), nullptr));
-        identifier = _getJsValue(jSObjectGetProperty(
-            globalContext,
-            obj,
-            jSStringCreateWithUTF8CString('identifier'.toNativeUtf8()),
-            nullptr));
-        var _options = jSObjectGetProperty(globalContext, obj,
-            jSStringCreateWithUTF8CString('options'.toNativeUtf8()), nullptr);
-        var options = jsObjectToDartMap(_options);
-
-        var onCreate = (Map<String, dynamic> args) async {};
-        var _onCreate = jSObjectGetProperty(globalContext, obj,
-            jSStringCreateWithUTF8CString('onCreate'.toNativeUtf8()), nullptr);
-        if (jSValueIsObject(globalContext, _onCreate) == 1) {
-          onCreate = (Map<String, dynamic> args) async {
-            var optionsObj = jscore.JSObject.make(
-                _globalContext,
-                jscore.JSClass.create(
-                    jscore.JSClassDefinition(className: "OptionsObj")));
-            for (var key in args.keys) {
-              var value = args[key];
-              if (value is String) {
-                optionsObj.setProperty(
-                    key,
-                    jscore.JSValue.makeString(_globalContext, value),
-                    jscore.JSPropertyAttributes.kJSPropertyAttributeReadOnly);
-              }else if(value is int || value is double){
-                optionsObj.setProperty(
-                    key,
-                    jscore.JSValue.makeNumber(_globalContext, value as double),
-                    jscore.JSPropertyAttributes.kJSPropertyAttributeReadOnly);
-              }
-            }
-            jSObjectCallAsFunction(
-                globalContext,
-                _onCreate,
-                obj,
-                1,
-                jscore.JSValuePointer.array([optionsObj.toValue()]).pointer,
-                nullptr);
-          };
-        }
-
-        templates.add(Template(
-          name,
-          description,
-          version,
-          options,
-          onCreate,
-          icon,
-          identifier,
-        ));
-      }
-    }
-    return nullptr;
-  }
-
-  /// # ======== THE PRINT FUNCTION - END ========== # ///
 
   /// Expose the CoreCoder class to js
   void initializeCC3JS() {
+    //TODO: this doesn't use JSCore, which is easier to read, see `initializeCC3JSFileIO`
     Pointer<Utf8> ccClassName = 'CoreCoder'.toNativeUtf8();
-
-    // Registering alert function
-    Pointer<Utf8> funcNameCString = 'alert'.toNativeUtf8();
-    contextGroup = jSContextGroupCreate();
-    // globalContext = jSGlobalContextCreateInGroup(contextGroup, nullptr);
-    _globalContext =
-        jscore.JSContext.createInGroup(group: jscore.JSContextGroup.create());
-    globalObject = jSContextGetGlobalObject(globalContext);
-
-    _alertDartFunc = _alert;
-    var functionObject = jSObjectMakeFunctionWithCallback(
-        globalContext,
-        jSStringCreateWithUTF8CString(funcNameCString),
-        Pointer.fromFunction(alert));
-    jSObjectSetProperty(
-        globalContext,
-        globalObject,
-        jSStringCreateWithUTF8CString(funcNameCString),
-        functionObject,
-        JSPropertyAttributes.kJSPropertyAttributeNone,
-        nullptr);
-    malloc.free(funcNameCString);
-
-    // Registering other function
-    _printDartFunc = _print;
-    _ccAddTemplateFunc = _ccAddTemplate;
 
     var staticFunctions = JSStaticFunctionPointer.allocateArray([
       JSStaticFunctionStruct(
         name: 'print'.toNativeUtf8(),
-        callAsFunction: Pointer.fromFunction(flutterPrint),
+        callAsFunction: Pointer.fromFunction(CoreCoder.flutterPrint),
         attributes: JSPropertyAttributes.kJSPropertyAttributeNone,
       ),
       JSStaticFunctionStruct(
         name: 'addTemplate'.toNativeUtf8(),
-        callAsFunction: Pointer.fromFunction(ccAddTemplate),
+        callAsFunction: Pointer.fromFunction(CoreCoder.jsAddTemplate),
         attributes: JSPropertyAttributes.kJSPropertyAttributeNone,
       ),
     ]);
@@ -347,15 +63,68 @@ class JsModule extends Module {
       convertToType: null,
     );
     var flutterJSClass = jSClassCreate(definition);
-    var flutterJSObject = jSObjectMake(globalContext, flutterJSClass, nullptr);
+    var flutterJSObject = jSObjectMake(_ctxPtr, flutterJSClass, nullptr);
     jSObjectSetProperty(
-        globalContext,
-        globalObject,
+        _ctxPtr,
+        _globalObjPtr,
         jSStringCreateWithUTF8CString(ccClassName),
         flutterJSObject,
         JSPropertyAttributes.kJSPropertyAttributeDontDelete,
         nullptr);
     malloc.free(ccClassName);
+  }
+
+  void initializeCC3JSFileIO() {
+    var staticFunctions = <jscore.JSStaticFunction>[
+      jscore.JSStaticFunction(
+          name: "writeFile",
+          attributes: jscore.JSPropertyAttributes.kJSPropertyAttributeReadOnly,
+          callAsFunction: Pointer.fromFunction(CoreCoder.flutterPrint)),
+      jscore.JSStaticFunction(
+          name: "appendFile",
+          attributes: jscore.JSPropertyAttributes.kJSPropertyAttributeReadOnly,
+          callAsFunction: Pointer.fromFunction(CoreCoder.flutterPrint)),
+      jscore.JSStaticFunction(
+          name: "readFile",
+          attributes: jscore.JSPropertyAttributes.kJSPropertyAttributeReadOnly,
+          callAsFunction: Pointer.fromFunction(CoreCoder.flutterPrint)),
+      jscore.JSStaticFunction(
+          name: "isExists",
+          attributes: jscore.JSPropertyAttributes.kJSPropertyAttributeReadOnly,
+          callAsFunction: Pointer.fromFunction(CoreCoder.flutterPrint)),
+      jscore.JSStaticFunction(
+          name: "isFile",
+          attributes: jscore.JSPropertyAttributes.kJSPropertyAttributeReadOnly,
+          callAsFunction: Pointer.fromFunction(CoreCoder.flutterPrint)),
+      jscore.JSStaticFunction(
+          name: "isDirectory",
+          attributes: jscore.JSPropertyAttributes.kJSPropertyAttributeReadOnly,
+          callAsFunction: Pointer.fromFunction(CoreCoder.flutterPrint)),
+    ];
+    var classDef = jscore.JSClassDefinition(
+      version: 0,
+      attributes: jscore.JSClassAttributes.kJSClassAttributeNone,
+      className: "FileIO",
+      parentClass: null,
+      staticValues: null,
+      staticFunctions: staticFunctions,
+      initialize: null,
+      finalize: null,
+      hasProperty: null,
+      getProperty: null,
+      setProperty: null,
+      deleteProperty: null,
+      getPropertyNames: null,
+      callAsFunction: null,
+      callAsConstructor: null,
+      hasInstance: null,
+      convertToType: null,
+    );
+    globalObj.setProperty(
+        "FileIO",
+        jscore.JSObject.make(context, jscore.JSClass.create(classDef))
+            .toValue(),
+        jscore.JSPropertyAttributes.kJSPropertyAttributeDontDelete);
   }
 
   //TODO: check if any parameter is invalid
@@ -374,28 +143,33 @@ class JsModule extends Module {
 
   @override
   void onInitialized(ModulesManager modulesManager) {
+    context =
+        jscore.JSContext.createInGroup(group: jscore.JSContextGroup.create());
+    globalObj = context.globalObject;
+    _globalObjPtr = globalObj.pointer;
+    CoreCoder.module = this;
+    FileIO.module = this;
+    CoreCoder.context = context;
+    FileIO.context = context;
     initializeCC3JS();
+    initializeCC3JSFileIO();
     debugPrint("Initializing JSModule $name");
     jSEvaluateScript(
-        globalContext,
+        _ctxPtr,
         jSStringCreateWithUTF8CString(mainScript.toNativeUtf8()),
         nullptr,
         nullptr,
         1,
         nullptr);
     Pointer onInitializedValueRef = jSObjectGetProperty(
-      globalContext,
-      globalObject,
+      _ctxPtr,
+      _globalObjPtr,
       jSStringCreateWithUTF8CString('onInitialized'.toNativeUtf8()),
       nullptr,
     );
     Pointer onInitializedObj =
-        jSValueToObject(globalContext, onInitializedValueRef, nullptr);
+        jSValueToObject(_ctxPtr, onInitializedValueRef, nullptr);
     jSObjectCallAsFunction(
-        globalContext, onInitializedObj, nullptr, 0, nullptr, nullptr);
-    // var global = jsContext!.globalObject;
-    // var onInitialized = global.getProperty("onInitialized").toObject();
-    // onInitialized.callAsFunction(global, JSValuePointer.array([]));
-    // debugPrint(hey);
+        _ctxPtr, onInitializedObj, nullptr, 0, nullptr, nullptr);
   }
 }
